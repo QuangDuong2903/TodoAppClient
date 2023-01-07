5a66ba28796aa20970e94133ab827a114b71ee59
Quang Duong <quangduongptsc@gmail.com>
2023-01-07 16:47:03 +0700

    test production

--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -24,8 +24,8 @@ function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
+            <Link color="inherit" href="https://github.com/QuangDuong2903">
+                QuangDuong2903
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
--- src/app/constant/apiURL.js
+++ src/app/constant/apiURL.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8081/api'
-
+// const BASE_URL = 'http://localhost:8081/api'
+const BASE_URL = 'https://todo-service-axio.onrender.com/api'
 export const LOGIN_URL = `${BASE_URL}/auth/signin`
 export const SIGNUP_URL = `${BASE_URL}/auth/signup`
 export const TASK_URL = `${BASE_URL}/task`
